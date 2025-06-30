15ab0e129081776f7a91f3c87e805cc8615e76c5
NithishKumar003 <enithishkumar91@gmail.com>
2025-06-30 15:31:22 +0000

    Add static root for render deployment

--- Blog/settings.py
+++ Blog/settings.py
@@ -124,11 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+import os
+
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "blog/static/blog/"
-]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
